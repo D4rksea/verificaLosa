85dca891a3ab49f053ae1f7bf132e5cc0368e4ba
D4rksea <65219077+D4rksea@users.noreply.github.com>
2020-05-20 13:12:23 +0200

    varie cose

--- news/models.py
+++ news/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # il giornalista è chiave esterna in articoli
+class Giornalista(models.Model):
+    nome = models.TextField()
+    cognome = models.TextField()
 
 class Articoli(models.Model):
     titolo = models.CharField(max_length=100)
     contenuto = models.TextField()
-    giornalista = models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name='comments')
-
-class Giornalista(models.Model):
-    nome = models.TextField()
-    cognome =models.TextField()
+    giornalista = models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name='articoli')
--- news/views.py
+++ news/views.py
@@ -1,26 +1,15 @@
 from django.shortcuts import render
-# from django.views.generic.list import listView
+from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Articoli, Giornalista
 
 def home(request):
-    blog_command = ['lista articoli','lista giornalisti']
-    context = {
-        'menu': blog_command,
-    }
-    return render(request, "/index.html", context)
+    return render(request, "home.html")
 
-def index(request):
-    blog_command = ['lista articoli','lista giornalisti']
-    context = {
-        'blog_command': blog_command,
-    }
-    return render(request, "verificaLosa/index.html", context)
-
-class PostDetailView(listView):
-    model = Articoli #modello dei dati da utilizzare
+class GiornalistiView(listView):
+    model = Giornalista #modello dei dati da utilizzare
     template_name = "templates/news/articoli.html" #pagina per mostrare i dati
 
-class PostDetailView2(listView):
-    model = Giornalista #modello dei dati da utilizzare
+class ArticoliView(listView):
+    model = Articoli #modello dei dati da utilizzare
     template_name = "templates/news/articoli.html" #pagina per mostrare i dati
